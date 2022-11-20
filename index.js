85cbb9fceef78fe85b53a48f25a4b382f973fe4c
nurhasan01 <115416939+nurhasan01@users.noreply.github.com>
2022-11-20 13:41:17 +0600

    first upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var showSide= document.getElementById('menu');
+var sideBar=document.getElementsById('side');
+
+showSide.addEventListener('click', sidefunc);
+
+
+function sidefunc(){
+  side.style.visibility = "visible";
+};
